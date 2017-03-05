b005f318a990377684dcc6a3315e3fe85a0c974e
Vijay Vepakomma <vijay.vepakomma@philips.com>
2017-03-05 10:53:43 -0500

    Add layout elements

--- GithubBrowser/Login.js
+++ GithubBrowser/Login.js
@@ -4,6 +4,7 @@ import {
   StyleSheet,
   Text,
   View,
+  TextInput,
   Image
 } from 'react-native';
 
@@ -12,8 +13,11 @@ export default class Login extends Component{
     render(){
         return(
                 <View style={styles.container}>
-                    <Image style={styles.logo} source={require('./Images/Octocat.png')}/>
-                   <Text> Login Component </Text>
+                   <Image style={styles.logo} source={require('./Images/Octocat.png')}/>
+                   <Text style={styles.heading}> Github Browser </Text>
+                   <TextInput style={styles.input} autoFocus="true" keyboardType="email-address"  placeholder="Github Username"/>
+                   <TextInput style={styles.input} secureTextEntry='true'  placeholder="Github Password"/>
+                
                 </View>
 
         );
@@ -22,16 +26,34 @@ export default class Login extends Component{
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: '#00DDFF',
+        backgroundColor: '#00EE55',
         borderColor: '#000000',
-        borderStyle: 'dashed',
+        borderStyle: 'solid',
         borderWidth: 1,
-        flex: 1
+        flex: 1,
+        paddingTop: 100,
+        alignItems: 'center'
     },
     logo:{
-        width: 100,
-        height: 100
+        width: 66,
+        height: 55
+    },
+    heading:{
+        fontSize:30
+    },
+    input:{
+        height:40,
+        marginTop:20,
+        marginLeft:30,
+        marginRight:30,
+        borderColor: '#000000',
+        borderStyle: 'solid',
+        backgroundColor: '#ffffff',
+        paddingLeft:10
+
     }
+
+    
 });
 
 
